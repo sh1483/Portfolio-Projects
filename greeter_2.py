7c5ce6c6aafd2b334b1b869915d4653eba87210d
Steven Haynes <93000820+sh1483@users.noreply.github.com>
2022-08-09 12:19:08 -0500

    Delete greeter_2.py

--- greeter_2.py
+++
@@ -1,9 +0,0 @@
-# Calling a function that contains a parameter (username) and an argument,
-# ('jesse').
-
-def greet_user(username):
-    """Display a simple greeting."""
-    print(f"Hello, {username.title()}!")
-
-greet_user('jesse')
-greet_user('sarah')
