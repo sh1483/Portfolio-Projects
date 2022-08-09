7c4d325885cf5b57f213948e2af7fd87f02b7fe0
Steven Haynes <93000820+sh1483@users.noreply.github.com>
2022-08-09 12:14:39 -0500

    Delete apostrophe.py

--- apostrophe.py
+++
@@ -1,7 +0,0 @@
-
-#Using quotes and an apostrophe correctly
-message = "One of Python's strengths is its diverse community."
-print(message)
-
-#Seeing how single quotes can impact a line of code.
-message= 'One of Python's strengths is its diverse community.''
