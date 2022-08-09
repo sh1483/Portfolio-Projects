a67ba44e1ed50280ae1534dd2b34b40f14adbff2
Steven Haynes <93000820+sh1483@users.noreply.github.com>
2022-08-09 12:05:08 -0500

    Delete 5_10.py

--- 5_10.py
+++
@@ -1,8 +0,0 @@
-current_users = ['jose','juan pablo','mario','manuel','victor','valeria','maria']
-new_users = ['Jose','juan pablo','marco','josue','enrique']
-
-for new_users in new_users:
-    if new_users.lower() in current_users:
-       print(f"Sorry, {new_users} that name has been taken. Enter a new user name.")
-    else:
-      print(f"{new_users} is available to use.")
