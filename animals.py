5662778c6e40278eefdc94926ffca115815dd9ad
Steven Haynes <93000820+sh1483@users.noreply.github.com>
2022-08-09 12:14:30 -0500

    Delete animals.py

--- animals.py
+++
@@ -1,5 +0,0 @@
-
-animals = ['elephant','ant eater','sloth']
-for animal in animals:
-	print(f'You know what would make a strange pet? An {animal.title()}!\n')
-print(f'All of these animals are a mammal.')
