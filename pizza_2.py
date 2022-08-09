13b0cf9100b3c7622b29d96e8f9ec31054da4f1f
Steven Haynes <93000820+sh1483@users.noreply.github.com>
2022-08-09 12:22:33 -0500

    Delete pizza_2.py

--- pizza_2.py
+++
@@ -1,6 +0,0 @@
-# Importing an entire module.
-def make_pizza(size, *toppings):
-    """Summarize the pizza we are about to make."""
-    print(f"\nMaking a {size}-inch pizza with the following toppings:")
-    for topping in toppings:
-        print(f"- {topping}")
