cf1e118d1930abad9fe7914eba451e41039f4270
Steven Haynes <93000820+sh1483@users.noreply.github.com>
2022-08-09 12:23:31 -0500

    Delete rollercoaster.py

--- rollercoaster.py
+++
@@ -1,9 +0,0 @@
-
-height = input("How tall are you, in inches?")
-height = int(height)
-
-if height >= 48:
-    print("\nYou are tall enough to ride!")
-else:
-    print("\nYou'll be able to ride when you are a little taller.")
-    
