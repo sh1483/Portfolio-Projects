86f500c1e372389e950b5d00289b07a2207c5894
Steven Haynes <93000820+sh1483@users.noreply.github.com>
2022-08-09 12:08:57 -0500

    Delete 7_3.py

--- 7_3.py
+++
@@ -1,9 +0,0 @@
-
-#Seeing if an input number is a multiple of 10
-number = input("Enter a number: ")
-number = int(number)
-
-if number % 10 == 0:
-    print(str(number) + " is divisible by 10.")
-else:
-    print(str(number) + " is not divisible by 10.")
